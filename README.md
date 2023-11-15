f940984269d97112b6333fdd984171d7cfad61cc
Paper890 <140361640+Paper890@users.noreply.github.com>
2023-11-15 19:52:48 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+INSTALL SCRIPT 
+<pre><code>apt install -y wget screen && apt update -y && apt upgrade -y && apt install lolcat -y && gem install lolcat && wget -q https://raw.githubusercontent.com/Paper890/san/main/setup.sh && chmod +x setup.sh && screen -S install ./setup.sh
+</code></pre>
+
+TESTED ON OS 
+- UBUNTU 20.04.05
+- DEBIAN 10
---
+++ ssh/cf.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Edition : Stable Edition V3.0
+# Auther  : Geo Project
+# (C) Copyright 2023
+# =========================================
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear
+apt install jq curl -y >/dev/null 2>&1
+read -rp "Sub Domain (Contoh: Myrid112): " -e sub
+DOMAIN=ridwanvpn.games
+SUB_DOMAIN=${sub}.ridwanvpn.games
+CF_ID=ziexyuan@gmail.com
+CF_KEY=2fbf0739b65e1e4b6de396b11a9693fc6aa56
+set -euo pipefail
+IP=$(wget -qO- ifconfig.me/ip);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "Host : $SUB_DOMAIN"
+echo "IP=" >> /var/lib/kyt/ipvps.conf
+echo $SUB_DOMAIN > /etc/xray/domain
+echo $SUB_DOMAIN > /root/domain
+rm -f /root/cf.sh

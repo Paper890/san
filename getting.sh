fe0a50a9257d7dc9db76b7e1cfc39396a4dd73e6
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-03-21 12:51:24 +0800

    Update getting.sh

--- getting.sh
+++ getting.sh
@@ -23,8 +23,7 @@ clear
 clear && clear && clear
 clear;clear;clear
 
-  # // Banner
-function pasang_domain() {
+  # // Banner & domain
 echo -e ""
 clear
 echo -e "---------------------------------------------------"
@@ -32,36 +31,21 @@ echo -e "     _______   _  __  ______________  ___  ____    "
 echo -e "    / __/ _ | / |/ / / __/_  __/ __ \/ _ \/ __/    "
 echo -e "   _\ \/ __ |/    / _\ \  / / / /_/ / , _/ _/      "
 echo -e "  /___/_/ |_/_/|_/ /___/ /_/  \____/_/|_/___/      "
-echo -e "  AUTOSCRIPT FOR VPN : SSH,VMESS,VLESS,TROJAN      "
-echo -e "  BUY PERMISION Wa.me : 085155208019               "
+echo -e "                                                   "
+echo -e "          BUY PERMISION : 085155208019             "
 echo -e "---------------------------------------------------"
 echo ""
-echo -e "   .----------------------------------."
-echo -e "   |\e[1;32mPlease Select a Domain Type Below \e[0m|"
-echo -e "   '----------------------------------'"
-echo -e "     \e[1;32m1)\e[0m Enter Your Subdomain"
-echo -e "   ------------------------------------"
-read -p "   Please select numbers 1-2 or Any Button(Random) : " host
+mkdir -p /etc/xray
+touch /etc/xray/domain
+echo -e "————————————————————————————————————————"
+echo -e "     ${YB}Silahkan Masukkan Domain Kamu${NC}         "
+echo -e "————————————————————————————————————————"
+read -p "Input domain kamu : " host1
 echo ""
-if [[ $host == "1" ]]; then
-echo -e "   \e[1;32mPlease Enter Your Subdomain $NC"
-read -p "   Subdomain: " host1
 echo "IP=" >> /var/lib/kyt/ipvps.conf
 echo $host1 > /etc/xray/domain
 echo $host1 > /root/domain
 echo ""
-elif [[ $host == "2" ]]; then
-#install cf
-wget ${REPO}ssh/cf.sh && chmod +x cf.sh && ./cf.sh
-rm -f /root/cf.sh
-clear
-else
-print_install "Random Subdomain/Domain is Used"
-wget ${REPO}cf.sh && chmod +x cf.sh && ./cf.sh
-rm -f /root/cf.sh
-clear
-    fi
-}
 
 clear
 # // Checking Os Architecture

632794d65fbd020cf954b5740afb04bdc3fe585a
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-03-21 12:56:27 +0800

    Update san.sh

--- san.sh
+++ san.sh
@@ -7,6 +7,6 @@ apt install lolcat -y
 gem install lolcat
 
 # Download and run setup.sh
-wget -q https://raw.githubusercontent.com/Paper890/san/main/getting.sh
-chmod +x getting.sh
-screen -S install ./getting.sh
+wget -q https://raw.githubusercontent.com/Paper890/san/main/get1.sh
+chmod +x get1.sh
+screen -S install ./get1.sh

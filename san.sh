0e424fc966649138bb514bb7eccd3b389c2a753e
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-03-25 06:43:39 +0800

    Update san.sh

--- san.sh
+++ san.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Install dependencies
-apt install -y wget screen
-apt update -y && apt upgrade -y
-apt install lolcat -y
-gem install lolcat
-
 # Download and run setup.sh
 wget -q https://raw.githubusercontent.com/Paper890/san/main/get1.sh
 chmod +x get1.sh

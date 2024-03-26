5117bf2850b3524d6854a77b852769371aff3a7d
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-03-26 10:29:08 +0800

    Update san.sh

--- san.sh
+++ san.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Download and run setup.sh
-wget -q https://raw.githubusercontent.com/Paper890/san/main/get1.sh
-chmod +x get1.sh
-screen -S install ./get1.sh
+wget -q https://raw.githubusercontent.com/Paper890/san/main/setup1.sh
+chmod +x setup1.sh
+screen -S install ./setup1.sh

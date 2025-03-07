656630b9e8ad276027f8c52879759de6ed3c4eca
Paper890 <140361640+Paper890@users.noreply.github.com>
2025-03-07 13:20:16 +0800

    Create renew_vless.sh

---
+++ web/renew_vless.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -rp "Input Username : " user
+read -p "Expired (days): " masaaktif
+
+ 
+    exp=$(grep -wE "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+    now=$(date +%Y-%m-%d)
+    d1=$(date -d "$exp" +%s)
+    d2=$(date -d "$now" +%s)
+    exp2=$(( (d1 - d2) / 86400 ))
+    exp3=$(($exp2 + $masaaktif))
+    exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
+    sed -i "/#& $user/c\#& $user $exp4" /etc/xray/config.json
+    systemctl restart xray > /dev/null 2>&1
+    
